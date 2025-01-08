86eef9e761d776df7d0d462725f72cf7e9d5fe28
rggua <rggua@ROMELLAPTOP>
2025-01-07 20:59:37 -0500

    primeras actualizaciones del programa

--- People/PersonRepository.cs
+++ People/PersonRepository.cs
@@ -1,4 +1,5 @@
-﻿using People.Models;
+﻿using SQLite;
+using People.Models;
 
 namespace People;
 
@@ -8,12 +9,16 @@ public class PersonRepository
 
     public string StatusMessage { get; set; }
 
-    // TODO: Add variable for the SQLite connection
+    private SQLiteConnection conn;
 
     private void Init()
     {
-        // TODO: Add code to initialize the repository         
-    }
+        if (conn != null)
+            return;
+
+        conn = new SQLiteConnection(_dbPath);
+        conn.CreateTable<Person>();
+    } 
 
     public PersonRepository(string dbPath)
     {
@@ -26,12 +31,14 @@ public class PersonRepository
         try
         {
             // TODO: Call Init()
+            Init();
 
             // basic validation to ensure a name was entered
             if (string.IsNullOrEmpty(name))
                 throw new Exception("Valid name required");
 
             // TODO: Insert the new person into the database
+            result = conn.Insert(new Person { Name = name });
             result = 0;
 
             StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
@@ -45,10 +52,10 @@ public class PersonRepository
 
     public List<Person> GetAllPeople()
     {
-        // TODO: Init then retrieve a list of Person objects from the database into a list
         try
         {
-            
+            Init();
+            return conn.Table<Person>().ToList();
         }
         catch (Exception ex)
         {
@@ -56,5 +63,5 @@ public class PersonRepository
         }
 
         return new List<Person>();
-    }
+    } 
 }
